8e88cf3780da861985dc010d97a4dc6968aa0a96
James Nash <james.nash-2@student.manchester.ac.uk>
2025-11-26 16:50:37 +0000

    Added FinBERT Sentiment Analyser

--- Backend/engine.py
+++ Backend/engine.py
@@ -1,12 +1,22 @@
 import trafilatura
+from transformers import pipeline
+
+sentiment_analyser = pipeline('text-classification', model='ProsusAI/finbert') #Finbert is trained for financial news
 
 def get_article(url):
-    download = trafilatura.fetch_url(url)
+    download = trafilatura.fetch_url(url) #Retrieves article from url
     if download is None:
         return "Error: Unable to fetch the URL."
-    text = trafilatura.extract(download)
+    text = trafilatura.extract(download) #Extracts the text from the article
     return text
+
+def get_sentiment(text):
+    #FinBERT can only read 512 at a time so let it seperate with 512 characters a time
+    result = sentiment_analyser(text, truncation=True, max_length=512)
+    return result[0]
+
 
 if __name__ == "__main__":
     url = "https://www.bbc.co.uk/news/articles/c1kpnxvpgy2o"
-    print(get_article(url))
+    text = get_article(url)
+    print(get_sentiment(text))
